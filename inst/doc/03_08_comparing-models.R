8b8fc2ca805a911ee2fe1ca061e5c70d746f1dfc
jtleek <jtleek@gmail.com>
2015-08-27 21:06:13 -0400

    big site update

---
+++ inst/doc/03_08_comparing-models.R
@@ -0,0 +1,54 @@
+## ----style, results = 'asis',include=FALSE-------------------------------
+BiocStyle::markdown()
+
+## ----global_options,include=FALSE----------------------------------------
+## see ch. 10 Hooks of Xie's knitr book
+knit_hooks$set(setPch = function(before, options, envir) {
+  if(before) par(pch = 19)
+})
+opts_chunk$set(setPch = TRUE)
+library(RSkittleBrewer)
+# Make the colors pretty
+trop = RSkittleBrewer("tropical")
+palette(trop)
+
+## ------------------------------------------------------------------------
+library(sva)
+library(bladderbatch)
+data(bladderdata)
+
+## ------------------------------------------------------------------------
+pheno = pData(bladderEset)
+edata = exprs(bladderEset)
+
+## ------------------------------------------------------------------------
+## Setting seed so the jitter will be the same
+set.seed(123)
+
+cancerjit = jitter(as.numeric(pheno$cancer))
+lm1 = lm(edata[1,] ~ 1)
+lm2 = lm(edata[1,] ~ pheno$cancer)
+
+par(mfrow=c(1,2))
+
+plot(edata[1,] ~ cancerjit,type="p",xaxt="n",xlab="Cancer Status",ylab="Expression",pch=19,col=as.numeric(pheno$cancer))
+axis(1,at=c(1,2,3),c("Biopsy","Cancer","Normal"))
+abline(lm1,col="darkgrey",lwd=5)
+
+
+plot(edata[1,] ~ cancerjit,type="p",xaxt="n",xlab="Cancer Status",ylab="Expression",pch=19,col=as.numeric(pheno$cancer))
+axis(1,at=c(1,2,3),c("Biopsy","Cancer","Normal"))
+boxplot(lm2$fitted~pheno$cancer,add=T,border=1:3)
+
+
+## ------------------------------------------------------------------------
+par(mfrow=c(1,2))
+plot(lm1$residuals ~ cancerjit,type="p",xaxt="n",xlab="Cancer Status",ylab="Expression",pch=19,col=as.numeric(pheno$cancer),ylim=c(-1.1,1.1))
+axis(1,at=c(1,2,3),c("Biopsy","Cancer","Normal"))
+
+plot(lm2$residuals ~ cancerjit,type="p",xaxt="n",xlab="Cancer Status",ylab="Residuals",pch=19,col=as.numeric(pheno$cancer),ylim=c(-1.1,1.1))
+axis(1,at=c(1,2,3),c("Biopsy","Cancer","Normal"))
+
+## ----session_info--------------------------------------------------------
+devtools::session_info()
+
